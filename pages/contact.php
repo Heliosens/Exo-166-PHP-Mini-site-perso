fd2d4fe712e3b73b4ea6a08c54940d1a3cb9873c
Sylvie-B <unsolo@hotmail.fr>
2022-01-27 11:55:54 +0100

    contact

--- pages/contact.php
+++ pages/contact.php
@@ -2,8 +2,19 @@
     <h1>Contact</h1>
 </section>
 <section>
-    <p>E-mail : monmail@hotmail.fr</p>
-    <p>tel : 06 xx xx xx xx</p>
+    <div class="content">
+        <form action="../form/save.php">
+            <div>
+                <label>Pseudo</label>
+                <input type="text" name="userName">
+            </div>
+            <div>
+                <label for="id-message">votre message</label>
+                <textarea name="userMessage" id="id-message" cols="30" maxlength="255" rows="10" placeholder="255 caracteres max."></textarea>
+            </div>
+            <div>
+                <input type="submit" value="envoyer">
+            </div>
+        </form>
+    </div>
 </section>
-
-
